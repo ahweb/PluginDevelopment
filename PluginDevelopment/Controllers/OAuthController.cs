c426769998f9d86f85cc3da7bde0b168e037eae6
pengze0902 <13797107305@163.com>
2017-02-25 00:22:04 +0800

    添加oatuh

---
+++ PluginDevelopment/Controllers/OAuthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace PluginDevelopment.Controllers
+{
+    public class OAuthController : ApiController
+    {
+        public HttpResponseMessage Get(string code)
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(code, Encoding.UTF8, "text/plain")
+            };
+        }
+
+
+        public HttpResponseMessage GetToken()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("", Encoding.UTF8, "text/plain")
+            };
+        }
+    }
+}
